5c592231906c3d52532d9958388292fcc21345fa
dougpdb <doug.pdb@me.com>
2013-01-14 05:44:11 -0600

    Subclass STTodayTVC

---
+++ Six Times Path/STPreviousDayTVC.h
@@ -0,0 +1,15 @@
+//
+//  STPreviousDayTVC.h
+//  Six Times Path
+//
+//  Created by Doug on 1/14/13.
+//  Copyright (c) 2013 A Great Highway. All rights reserved.
+//
+
+#import "STTodayTVC.h"
+
+@interface STPreviousDayTVC : STTodayTVC
+
+
+
+@end
---
+++ Six Times Path/STPreviousDayTVC.m
@@ -0,0 +1,39 @@
+//
+//  STPreviousDayTVC.m
+//  Six Times Path
+//
+//  Created by Doug on 1/14/13.
+//  Copyright (c) 2013 A Great Highway. All rights reserved.
+//
+
+#import "STPreviousDayTVC.h"
+
+@interface STPreviousDayTVC ()
+
+@end
+
+@implementation STPreviousDayTVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+	
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
